be110e2ab89554f718ee7f3ddc3502a837efab54
Pretty <prettythapa5@gmail.com>
2022-12-01 00:14:57 +0530

    Revert "Added REST/CRUD controllers for Beer"
    This reverts commit 01414330f3c38409366247e05f64c51379861edc.

--- mssc-beer-service/src/main/java/pretty/springframework/msscBeerService/web/model/BeerPagedList.java
+++ mssc-beer-service/src/main/java/pretty/springframework/msscBeerService/web/model/BeerPagedList.java
@@ -1,21 +1,7 @@
 package pretty.springframework.msscBeerService.web.model;
 
-import java.util.List;
-
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 
 public class BeerPagedList extends PageImpl<BeerDto>{
-
-	public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
-		super(content, pageable, total);
-		// TODO Auto-generated constructor stub
-	}
-	public BeerPagedList(List<BeerDto> content) {
-		super(content);
-		// TODO Auto-generated constructor stub
-	}
-
-
 
 }
