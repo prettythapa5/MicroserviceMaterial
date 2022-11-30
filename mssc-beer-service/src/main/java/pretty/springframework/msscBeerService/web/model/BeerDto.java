c4fd03dde805ab916b638af15ed22f7800cc89b3
Pretty <prettythapa5@gmail.com>
2022-12-01 00:28:54 +0530

    Reverting back to the proper repo or commit ID - 8bf5b60

--- mssc-beer-service/src/main/java/pretty/springframework/msscBeerService/web/model/BeerDto.java
+++ mssc-beer-service/src/main/java/pretty/springframework/msscBeerService/web/model/BeerDto.java
@@ -4,6 +4,15 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BeerDto {
 	private UUID id;
 	private Integer version;
