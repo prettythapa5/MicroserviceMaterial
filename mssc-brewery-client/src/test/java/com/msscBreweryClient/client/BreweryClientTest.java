4dd2931cd61a2744047acc10c76d87f5e90de946
Pretty <prettythapa5@gmail.com>
2022-12-07 00:18:08 +0530

    test case for post BeerDto added

--- mssc-brewery-client/src/test/java/com/msscBreweryClient/client/BreweryClientTest.java
+++ mssc-brewery-client/src/test/java/com/msscBreweryClient/client/BreweryClientTest.java
@@ -2,6 +2,7 @@ package com.msscBreweryClient.client;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.net.URI;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -15,12 +16,26 @@ class BreweryClientTest {
 
 	@Autowired
 	BreweryClient client;
-	
+
 	@Test
 	void testGetBeerById() {
 		BeerDto dto = client.getBeerById(UUID.randomUUID());
-		//fail("Not yet implemented");
+		// fail("Not yet implemented");
 		assertNotNull(dto);
+	}
+
+	@Test
+	void testSaveNewBeer() {
+		BeerDto beerDto = BeerDto.builder().id(UUID.randomUUID()).beerName("New Beer Set").beerStyle("Strong").upc(12L)
+				.build();
+		URI uri = client.saveNewBeer(beerDto);
+		System.out.println("Printing beerDto ====> " + beerDto);
+		System.out.println("Printing URI path ====> " + uri.getPath());
+
+		assertNotNull(uri);
+		
+
+		// System.out.println("Printing URi in testCase2 ====> "+uri.toString());
 	}
 
 }
